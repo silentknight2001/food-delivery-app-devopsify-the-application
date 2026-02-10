5d8c4446f9969ffca3864e9d8f7e3410038049ec
silentknight2001 <silentknight.www@gmail.com>
2026-02-10 22:16:04 +0530

    edit sh file

--- Ansible/scripts/dev.sh
+++ Ansible/scripts/dev.sh
@@ -1,20 +1,18 @@
 #!/usr/bin/env bash
 
-sudo apt remove -y docker docker-engine docker.io containerd runc
-sudo apt update
-sudo apt install -y ca-certificates curl gnupg
-sudo install -m 0755 -d /etc/apt/keyrings
+ apt remove -y docker docker-engine docker.io containerd runc
+ apt update
+ apt install -y ca-certificates curl gnupg
+ install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | \
-sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-sudo chmod a+r /etc/apt/keyrings/docker.gpg
+ gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+ chmod a+r /etc/apt/keyrings/docker.gpg
 echo \
 "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] \
 https://download.docker.com/linux/ubuntu \
 $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
 sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt update
-sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-sudo usermod -aG docker $USER
+ apt update
+ apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+ usermod -aG docker $USER
 docker --version
-
-
